889b2e11f220adef238f02c26c98fcfc1f50168f
KaneishaR <kaneisha.reese@gmail.com>
2020-07-18 09:43:30 -0500

    angular

--- redux/src/remote/toystore-client.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const toyStoreClient = axios.create({
-	baseURL: 'http://localhost:8080',
-	//baseURL: 'http://project2api-env.eba-t2d87vnp.us-east-2.elasticbeanstalk.com',
-	headers: {
-		'Content-Type': 'application/json'
-	}
-})
